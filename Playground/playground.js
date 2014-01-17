a8e7027965388f51f17012c2b64bfab5f68b800b
Tristan Cartledge <tristan.cartledge@gmail.com>
2014-01-17 16:55:46 +0000

    start of zip support

--- Playground/playground.js
+++ Playground/playground.js
@@ -1,5 +1,7 @@
 var Express = require("express");
 var extend = require("xtend");
+var fs = require("fs");
+var zip = require("express-zip");
 
 var cConfig = require(process.cwd() + "/config.json");
 
@@ -16,12 +18,24 @@ cApp.engine(".ejsh", require("ejs").__express);
 cApp.set('views', __dirname + '/views');
 
 cApp.use(Express.static(__dirname + '/static'));
+
+/*cApp.use("/Game/game.zip", function(cReq, cRes, fNext){
+    
+    fs.readdir(process.cwd(), function(cErr, aFiles){
+        
+    });
+    
+    fNext();
+});*/
+
 cApp.use("/Game", Express.static(process.cwd(), { index: "main.html" }));
+
 cApp.use("/", function(cReq, cRes){
     cRes.render("index.ejsh", {
         title: cPlaygroundConfig.title + " Playground"
     });
 });
+
 cApp.use(Express.favicon());
 
 var sPort = cPlaygroundConfig.playground.port;
