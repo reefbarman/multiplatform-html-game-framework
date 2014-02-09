b42018cc07a1f27841153e8054ee36aa087a58d7
Adam <adamwhiteland@googlemail.com>
2014-02-09 14:52:31 +0000

    fixes collision test

--- Source/collision/collisionutils.js
+++ Source/collision/collisionutils.js
@@ -8,8 +8,10 @@ EN.CollisionUtils = (function(){
         TestEntityIntersect: function(cCollidable1, cCollidable2){
             var cBounds1 = cCollidable1.GetBounds();
             var cBounds2 = cCollidable2.GetBounds();
+
+            var bCollision = !(cBounds1.x1 > cBounds2.x2 || cBounds1.y1 > cBounds2.y2 || cBounds1.x2 < cBounds2.x1 || cBounds1.y2 < cBounds2.y1);
             
-            return (!(cBounds1.x1 > cBounds2.x2 || cBounds1.y1 > cBounds1.y2 || cBounds1.x2 < cBounds2.x1 || cBounds1.y2 < cBounds2.y1));
+            return bCollision;
         }
     };
 })();
